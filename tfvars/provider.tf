1a8b827db6513fb9e7a468c3eaff141381b9fb23
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-04-16 22:39:48 -0400

    Initial Terraform Commit

---
+++ tfvars/provider.tf
@@ -0,0 +1,21 @@
+terraform{
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+    }
+
+        backend "s3" {
+     
+    }
+}
+
+#Configure the AWS provider
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+ 
+
